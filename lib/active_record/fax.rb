c98ac3eca29ee1e5f4554bb7f2223be945b60202
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2016-06-15 17:59:16 +0200

    New gem.

---
+++ lib/active_record/fax.rb
@@ -0,0 +1,7 @@
+require "active_record/fax/version"
+
+module ActiveRecord
+  module Fax
+    # Your code goes here...
+  end
+end
---
+++ lib/active_record/fax/version.rb
@@ -0,0 +1,5 @@
+module ActiveRecord
+  module Fax
+    VERSION = "0.1.0"
+  end
+end

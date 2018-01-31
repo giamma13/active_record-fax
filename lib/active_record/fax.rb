197ce56a7428fd6851b169c60549bf31b3020256
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2018-01-31 16:44:32 +0100

    Add incremental copy of databases through ssh tunnels.

--- lib/active_record/fax.rb
+++ lib/active_record/fax.rb
@@ -28,6 +28,12 @@ module ActiveRecord
     #   database: production
     #   socket: /tmp/mysql.sock
 
+    class Source < ActiveRecord::Base
+    end
+
+    class Destination < ActiveRecord::Base
+    end
+
     def self.reload!
       @configs = Hashie::Mash.new(YAML.load_file(File.join(Rails.application.root, 'config', 'database.yml')))
     end
@@ -37,6 +43,34 @@ module ActiveRecord
       src = config(source_db, :source)
       dst = config(dest_db, :destination)
       %x{ssh #{src.server_username}@#{src.server} 'mysqldump --skip-lock-tables --single-transaction -h#{src.host} -u#{src.username} -p"#{src.password}" #{src.database} | gzip -9' | gzip -cd | mysql -u#{dst.username} -p#{dst.password} #{dst.database}}
+    end
+
+    def self.incremental_copy(source_db, dest_db)
+      reload!
+      src = config(source_db, :source)
+      dst = config(dest_db, :destination)
+      Destination.establish_connection(dst)
+      dst_ids = max_ids(Destination)
+      src_ids = {}
+      with_ssh_tunnel(src['server_username'], src['server'], 3307, src['host'], 3306) do
+        conf = src.dup.tap do |db|
+          db['host'] = '127.0.0.1'
+          db['port'] = 3307
+        end
+        Source.establish_connection(conf)
+        src_ids = max_ids(Source)
+      end
+      dst_ids.map do |table_name, dst_id|
+        src_id   = src_ids[table_name]
+        cmd_base = %{ssh #{src.server_username}@#{src.server} 'mysqldump --skip-lock-tables --single-transaction -h#{src.host} -u#{src.username} -p\\#{src.password.split('').join('\\')}}
+        cmd_incr = %{--skip-add-drop-table --no-create-info --insert-ignore}
+        cmd_id   = %{--where "id > #{dst_id}"}
+        cmd_src  = %{#{src.database}}
+        cmd_dst  = %{| gzip -9' | gzip -cd | mysql -u#{dst.username} -p#{dst.password} #{dst.database}}
+        if dst_id && src_id && src_id > dst_id
+          [ cmd_base, cmd_incr, cmd_id, cmd_src, table_name, cmd_dst ].join(' ').strip
+        end
+      end.compact
     end
 
     def self.sync(source_db, dest_db, time, small_threshold)

82842a6f36b8481032635c7638aaa00cbc8e98fc
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2018-01-31 12:40:56 +0100

    Add table sync feature.

--- lib/active_record/fax.rb
+++ lib/active_record/fax.rb
@@ -39,6 +39,25 @@ module ActiveRecord
       %x{ssh #{src.server_username}@#{src.server} 'mysqldump --skip-lock-tables --single-transaction -h#{src.host} -u#{src.username} -p"#{src.password}" #{src.database} | gzip -9' | gzip -cd | mysql -u#{dst.username} -p#{dst.password} #{dst.database}}
     end
 
+    def self.sync(source_db, dest_db, time, small_threshold)
+      reload!
+      src              = config(source_db, :source)
+      dst              = config(dest_db, :destination)
+      db_info          = remote_table_info(source_db)
+      incr_tables      = db_info.select { |k, v| v[:columns].map(&:to_sym).include?(:created_at) && v[:count].to_i > small_threshold }.keys
+      whole_tables     = db_info.select { |k, v| !incr_tables.include?(k.to_sym) }.keys
+      cmd_base         = %{ssh #{src.server_username}@#{src.server} 'mysqldump --skip-lock-tables --single-transaction -h#{src.host} -u#{src.username} -p\\#{src.password.split('').join('\\')}}
+      cmd_time         = %{--where "created_at > \\"#{time}\\""}
+      cmd_incr         = %{--skip-add-drop-table --no-create-info --insert-ignore}
+      cmd_src          = %{#{src.database}}
+      cmd_incr_tables  = %{#{incr_tables.map(&:to_s).join(' ')}}
+      cmd_whole_tables = %{#{whole_tables.map(&:to_s).join(' ')}}
+      cmd_dst          = %{| gzip -9' | gzip -cd | mysql -u#{dst.username} -p#{dst.password} #{dst.database}}
+      incr_cmd         = [ cmd_base, cmd_time, cmd_incr, cmd_src, cmd_incr_tables, cmd_dst ].join(' ').strip
+      whole_cmd        = [ cmd_base, cmd_src, cmd_whole_tables, cmd_dst ].join(' ').strip
+      [ incr_cmd, whole_cmd ]
+    end
+
     def self.config(name, purpose)
       checks  = %w(username password database)
       checks += %w(server host) if purpose == :source
@@ -54,6 +73,37 @@ module ActiveRecord
 
     def self.destinations
       @configs.keys - sources
+    end
+
+    def self.with_ssh_tunnel(gateway_user, gateway_host, local_port, remote_host, remote_port, &block)
+      gateway = Net::SSH::Gateway.new(gateway_host, gateway_user)
+      gateway.open(remote_host, remote_port, local_port)
+      yield
+      gateway.shutdown!
+    end
+
+    def self.remote_table_info(env_name)
+      env_name = env_name.to_sym
+      db_conf  = config(env_name.to_sym, :source).dup
+      @table_info ||= {}
+      @table_info[env_name] ||= {}
+      begin
+        with_ssh_tunnel(db_conf['server_username'], db_conf['server'], 3307, db_conf['host'], 3306) do
+          db_conf['host'] = '127.0.0.1'
+          db_conf['port'] = 3307
+          ActiveRecord::Base.establish_connection(db_conf)
+          conn   = ActiveRecord::Base.connection
+          conn.tables.each do |t|
+            @table_info[env_name][t.to_sym] = {
+              count: (conn.execute("SELECT MAX(id) FROM #{t}").to_a.flatten[0] rescue nil),
+              columns: conn.execute("SHOW COLUMNS FROM #{t}").to_a.map { |c| c[0] },
+            }
+          end
+        end
+      ensure
+        ActiveRecord::Base.establish_connection(config(Rails.env, :destination))
+      end
+      @table_info[env_name]
     end
 
     def self.define_methods!
